2f41b11db049b369f1c8eb2ea957cd515286e7e6
Pallavi <pallavipawar0063@gmail.com>
2026-09-24 19:15:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //complete this code
 class Person {
-	constructor(name, age)
-	this._name = name;
-	this._age = age;
+	constructor(name, age){
+		this._name = name;
+		this._age = age;
+	}
+	
 	get name(){
 		return this._name;
 	}
